3d56ccec60c4fb498b1babafeda8cb670ffbdb55
Tucker SW <44379127+21tstrow@users.noreply.github.com>
2019-05-14 14:03:32 -0500

    Update SMART DUMMY.py

--- SMART DUMMY.py
+++ SMART DUMMY.py
@@ -10,85 +10,25 @@ RED = (255, 0, 0)
 PURPLE = (255, 0, 255)
 RED2 = (200, 50, 50)
 PINK = (250, 150, 150)
-
-#images
-global susuc
-global subfail
-
-'''
-Subsidize Farming -> decrease government wealth, increase food
-Take land -> decrease military might, increase resource diversity proportional to previous military might, increase pop happiness, decrease foreign relations, increase government wealth.
-Import -> decrease government wealth, increase resource diversity or food
-Export -> decrease food or resource diversity, increase government wealth
-Take over country -> if military might is greater than neighbouring country, gain 25% to every category but lose % military might proportional to neighbouring country's military might vs. own and 25% foreign relations; else, lose 50% military might and resource diversity
-Increase innovation -> decrease government wealth and food, increase resource diversity and population happiness
-Accept immigrants -> decrease food and government wealth, increase pop. and foreign_relations
-Gift -> decrease government wealth, increase foriegn relations
-Deport citizens -> decrease pop. and population happiness, increase food resource and government wealth
-Draft citizens -> decrease pop. and pop happiness, increase military might
-Hold festival -> decrease government wealth, increase population happiness
-Increase taxes -> decrease pop happiness, increase government wealth proportional to pop.
-'''
-population_happiness = 30
-military_might = 80
-
-food_resource = 25
-foreign_relations = 30
-total_population = 35
-government_wealth = 50
-resource_diversity = 40
-
-variables = [population_happiness, military_might, food_resource, foreign_relations, total_population, government_wealth, resource_diversity]
-
-def display_values():
-    for var in variables:
-        if var == population_happiness:
-            print_squares(500, 75, population_happiness)
-        if var == military_might:
-            print_squares(500, 125, military_might)
-        if var == food_resource:
-            print_squares(500, 180, food_resource)
-        if var == foreign_relations:
-            print_squares(500, 235, foreign_relations)
-        if var == total_population:
-            print_squares(500, 290, total_population)
-        if var == government_wealth:
-            print_squares(500, 340, government_wealth)
-        if var == resource_diversity:
-            print_squares(500, 395, resource_diversity)
-
-        
-def print_squares(x, y, value):
-    tempval = value
-
-    xval = x
-    yval = y
-    for i in range(10):
-        if value > 0:
-            pygame.draw.rect(screen, RED2, (xval, yval, 10, 10))
-        if value <= 0:
-            pygame.draw.rect(screen, WHITE, (xval, yval, 10, 10))
-        value -= 10
-        xval += 20
-    
+BUTTONBLUE = (0, 58, 254)
 
 pygame.init()
 
-screen = pygame.display.set_mode([800,800])
+screen = pygame.display.set_mode([1920, 1050])
 pygame.display.set_caption('Country Simulator')
 
-button_positionsx = [10, 145, 280, 415, 550, 685, 10, 145, 280, 415, 550, 685]
-button_positionsw = [250, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90]
-button_positionsy = [10, 535, 535, 535, 535, 535, 655, 655, 655, 655, 655, 655]
-button_positionsh = [150, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70, 70]
-button_colors = [GREEN, RED2, RED2, RED2, RED2, RED2, RED2, RED2, RED2, RED2, RED2, RED2]
-button_colsOG = [GREEN, RED2, RED2, RED2, RED2, RED2, RED2, RED2, RED2, RED2, RED2, RED2]
+button_positionsx = [20, 20, 20, 20, 1000, 1000, 1000, 1000]
+button_positionsw = [900, 900, 900, 900, 900, 900, 900, 900]
+button_positionsy = [300, 490, 680, 870, 300, 490, 680, 870]
+button_positionsh = [150, 150, 150, 150, 150, 150, 150, 150]
+button_colors = [BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE]
+button_colsOG = [BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE, BUTTONBLUE]
+button_text = ['Scenarios', 'Scenarios', 'Scenarios', 'Scenarios', 'Scenarios', 'Scenarios', 'Scenarios', 'Scenarios']
 
 ###############
 'Functions'
 ###############
 
-    
 
 def enter_number(variable):
     global numkeydone
@@ -126,9 +66,6 @@ def enter_number(variable):
     return numberused
 
 
-
-
-
 def runfxn(c):
     if c == 0:
         ''
@@ -159,10 +96,11 @@ def runfxn(c):
         
 def dispfxn(fxnnum):
     if fxnnum == 0:
-        print("yaya")
+        ''
         #Display fxn in info tab
 
-myfont = pygame.font.SysFont('Comic Sans MS', 30)
+titlefont = pygame.font.SysFont('Times New Roman', 100)
+subtitlefont = pygame.font.SysFont('Times New Roman', 80)
 #textsurface = myfont.render('Some Text', False, (0, 0, 0)) #Text Here
 #screen.blit(textsurface,(0,0))
 
@@ -181,8 +119,6 @@ while not done:
             mousex, mousey = get_pos
             if (mousex > (button_positionsx[i]) and  (mousey > button_positionsy[i]) and (mousex < (button_positionsx[i] + button_positionsw[i])) and (mousey < button_positionsy[i] + button_positionsh[i])):
                 button_colors[i] = PINK
-                print("yay")
-                print(get_pos)
             else:
                 button_colors[i] = button_colsOG[i]
 
@@ -196,10 +132,15 @@ while not done:
  
 
         #Display the Buttons
+        pygame.draw.rect(screen, (205, 205, 205), (0, 0, 1920, 1050))
         for i in range(len(button_positionsx)):
             pygame.draw.rect(screen, button_colors[i], (button_positionsx[i], button_positionsy[i], button_positionsw[i], button_positionsh[i]))
- 
-        display_values()
+            textsurface = subtitlefont.render(button_text[i], False, (255, 255, 255)) #Text Here
+            screen.blit(textsurface,(button_positionsx[i] + 250,button_positionsy[i] + 30))
+
+        textsurface = titlefont.render('Smart Dummy', False, (0, 0, 0)) #Text Here
+        screen.blit(textsurface,(700,100))
+        
  
 pygame.quit()
 
