f34a7cb7c611c493180b41ef989e461b8798601e
Tucker SW <44379127+21tstrow@users.noreply.github.com>
2019-05-17 14:03:22 -0500

    Update SMART DUMMY.py

--- SMART DUMMY.py
+++ SMART DUMMY.py
@@ -15,7 +15,7 @@ BUTTONBLUE = (0, 58, 254)
 pygame.init()
 
 screen = pygame.display.set_mode([1920, 1050])
-pygame.display.set_caption('Country Simulator')
+pygame.display.set_caption('SMART DUMMY')
 
 button_positionsx = [635, 955, 1275, 1595, 635, 955, 1275, 1595]
 button_positionsy = [50, 50, 50, 50, 600, 600, 600, 600]
@@ -23,11 +23,29 @@ button_positionsw = [290, 290, 290, 290, 290, 290, 290, 290]
 button_positionsh = [400, 400, 400, 400, 400, 400, 400, 400]
 button_colors = [BUTTONBLUE, GREEN, RED, PURPLE, PURPLE, RED, GREEN, BUTTONBLUE]
 button_colsOG = [BUTTONBLUE, GREEN, RED, PURPLE, PURPLE, RED, GREEN, BUTTONBLUE]
-button_text = ['Save', 'Save', 'Save', 'Save', 'Save', 'Save', 'Save', 'Save']
+button_text = ['Save 1', 'Save 2', 'Save 3', 'Save 4', 'Save 5', 'Save 6', 'Save 7', 'Save 8']
 
 ###############
 'Functions'
 ###############
+
+def ask(screen, question):
+    "ask(screen, question) -> answer"
+    pygame.font.init()
+    current_string = ''
+    display_box(screen, question + ": " + current_string)
+    inkey = 128
+    while True:
+      pygame.event.get()
+      if event.key != K_LEFT:
+        print(pygame.key.name(event.key))
+
+
+def display_box(screen, qa):
+    pygame.draw.rect(screen, (100,100,100), (0,0,800,800))
+    textsurface = smallbuttonfont.render(qa, False, (0, 0, 0))
+    screen.blit(textsurface,(0,0))
+    pygame.display.update()
 
 
 def enter_number(variable):
@@ -66,31 +84,53 @@ def enter_number(variable):
     return numberused
 
 
-def runfxn(c):
-    if c == 0:
-        ''
-    if c == 1:
-        ''
-    if c == 2:
-        ''
-    if c == 3:
-        ''
-    if c == 4:
-        ''
-    if c == 5:
-        ''
-    if c == 6:
-        ''
-    if c == 7:
-        ''
-    if c == 8:
-        ''
-    if c == 9:
-        ''
-    if c == 10:
-        ''
-    if c == 11:
-        ''
+def runfxn(c,title):
+    if title == 'save button':
+        if c == 0:
+            ''
+        if c == 1:
+            ask(screen, "Does this work? (y/n)")
+        if c == 2:
+            ''
+        if c == 3:
+            ''
+        if c == 4:
+            ''
+        if c == 5:
+            ''
+        if c == 6:
+            ''
+        if c == 7:
+            ''
+        if c == 8:
+            ''
+    else:
+        if title == 'sidebar':
+            if c == 0:
+                ''
+            if c == 1:
+                ''
+            if c == 2:
+                ''
+            if c == 3:
+                ''
+            if c == 4:
+                ''
+            if c == 5:
+                ''
+            if c == 6:
+                ''
+            if c == 7:
+                ''
+            if c == 8:
+                ''
+            if c == 9:
+                ''
+            if c == 10:
+                ''
+
+
+
     pygame.time.wait(2000)
         
         
@@ -113,7 +153,7 @@ sbt = ["Settings", "Diagnostics", "", "", "", "", "", "Save Data", "Scenario Des
 counter = 10
 while counter >= 1:
     sbx.append(50)
-    sby.append(100 * counter - 25)
+    sby.append(100 * counter - 60)
     sbw.append(500)
     sbh.append(50)
     sbc.append(WHITE)
@@ -145,11 +185,15 @@ while not done:
 
         pygame.display.update()
         
-        for i in range(len(button_positionsx)):
-            if (button_colors[i] == PINK) and (pygame.mouse.get_pressed() == (True, False, False)):
-                runfxn(i)
-            elif (button_colors[i] == PINK) and (pygame.mouse.get_pressed() == (False, False, False)):
-                dispfxn(i)
+        for button in range(len(button_positionsx)):
+            if (button_colors[button] == (30,30,30)) and (pygame.mouse.get_pressed() == (True, False, False)):
+                runfxn(button, 'save button')
+                print("n")
+
+        for sidebar in range(len(sbx)):
+            if (sbc[sidebar] == (200,200,200)) and (pygame.mouse.get_pressed() == (True, False, False)):
+                runfxn(sidebar,'sidebar')
+                print("y")
  
         pygame.draw.rect(screen, (0, 0, 0), (0, 0, 1920, 1050))
 
